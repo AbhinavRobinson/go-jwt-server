314739de4e6987ea773ed48904e306c3699565ce
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-04-29 10:04:19 +0000

    (feat): add server token auth

--- server/main.go
+++ server/main.go
@@ -5,14 +5,41 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	// add code
+	fmt.Fprintf(w, "secret information")
+}
+
+var mySigningKey = []byte("password")
+
+func isAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header["Token"] != nil {
+			token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("Error")
+				}
+				return mySigningKey, nil
+			})
+
+			if err != nil {
+				fmt.Fprintf(w, err.Error())
+			}
+
+			if token.Valid {
+				endpoint(w, r)
+			}
+		} else {
+			fmt.Fprintf(w, "Not Authorized")
+		}
+	})
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
+	http.Handle("/", isAuthorized(homePage))
 	port := 9000
 	fmt.Printf("Booting up server on port:%d", port)
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), nil))
